e621fa137d9bcee84a012d03626aede438fae522
Aliciea D Suechin <adr330@nyu.edu>
2019-06-20 00:44:40 +0000

    Automatically backed up by Learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -12,7 +12,7 @@
 # "Graduation Failed" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/07.mp3'
 # }
 
-def help(songs)
+def help
   #this method should be the same as in jukebox.rb
   
   songs  = [
