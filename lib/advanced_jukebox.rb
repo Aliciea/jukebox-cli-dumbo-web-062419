c524a86c9fa805306c972fe15bcb3ff777b6ac69
Aliciea D Suechin <adr330@nyu.edu>
2019-06-20 00:52:20 +0000

    Automatically backed up by Learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -73,26 +73,5 @@ end
 
 def run(my_songs)
   #this method is the same as in jukebox.rb
-  command = ""
-  while command
-  puts "Please enter a command:"
-  command = gets.downcase.strip
-  case command
-    when 'list'
-      list(songs)
-      when 'play'
-        list(songs)
-        play(songs)
-      when 'help'
-        help
-      when 'exit'
-        exit_jukebox
-        break
-      else
-        help
-      end
-    end
-    my_songs
   end 
-  
 end
